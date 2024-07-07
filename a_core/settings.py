912f13457898a75b9eb0da4077af3bc186404cdb
Davi <96668516+DaviRodrigues80@users.noreply.github.com>
2024-07-07 12:12:31 -0300

    Deploy 1.2

--- a_core/settings.py
+++ a_core/settings.py
@@ -159,7 +159,7 @@ DATABASES = {
     }
 }
 
-POSTGRES_LOCALLY = False
+POSTGRES_LOCALLY = True
 if ENVIRONMENT == 'production' or POSTGRES_LOCALLY == True:
     DATABASES['default'] = dj_database_url.parse(env('DATABASE_URL'))
 
